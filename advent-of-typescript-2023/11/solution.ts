0385a7ea52761c6a988975d372794b40f6a42194
Elliot Gibson <elliot.gibson@loyaltylogistix.com>
2023-12-11 13:24:55 +0000

    feat/better solution to day 11

--- advent-of-typescript-2023/11/solution.ts
+++ advent-of-typescript-2023/11/solution.ts
@@ -1,13 +1,5 @@
-export type DeepReadonly<TInput> = TInput extends Record<PropertyKey, unknown>
-	? ReadonlyObj<TInput>
-	: ReadonlyArr<TInput>;
+type NonAccessible = Function | boolean | number | string;
 
-type ReadonlyObj<TInput> = TInput extends Record<PropertyKey, unknown>
-	? { readonly [TKey in keyof TInput] : DeepReadonly<TInput[TKey]> }
-	: TInput;
-
-type ReadonlyArr<TInput> = TInput extends [infer THead, ...infer TTail] 
-	? readonly [DeepReadonly<THead>, ...DeepReadonly<TTail>]
-	: TInput;
-
-export type SantaListProtector<TInput> = DeepReadonly<TInput>
+export type SantaListProtector<TInput> = TInput extends NonAccessible
+	? TInput
+	: { readonly [TKey in keyof TInput] : SantaListProtector<TInput[TKey]>}
